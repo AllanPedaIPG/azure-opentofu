c43491de6313e20d304f776573add0ea7ba6aa7c
Allan Peda <78157665+AllanPedaIPG@users.noreply.github.com>
2023-10-12 15:19:12 -0400

    Create main.tf
    Minimal terraform file to create a resource group

---
+++ sample_rg/main.tf
@@ -0,0 +1,38 @@
+# October 11 2023
+# Allan Peda <allan.peda@interpublic.com>
+
+# For tenant and subscription, respectively
+# az account show --query 'homeTenantId' -o tsv
+# az account list -o table
+
+# Client ID and secret created through Azure Portal
+#   https://www.programonaut.com/how-to-easily-create-an-azure-service-principal-step-by-step/
+# CLI instructions for secret below were NG
+#   https://learn.microsoft.com/en-us/azure/developer/terraform/authenticate-to-azure?tabs=bash
+
+# started with this (note version lock must be in required_providers)
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.75"
+    }
+  }
+}
+
+# be sure this is set/exported in your environment
+# ARM_CLIENT_ID=aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee
+# ARM_CLIENT_SECRET=¯\_(ツ)_/¯_¯\_(ツ)_/¯_¯\_(ツ)_/¯_
+# ARM_SUBSCRIPTION_ID=aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee
+# ARM_TENANT_ID=aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "sndbx_rg"
+  location = "eastus"
+}
